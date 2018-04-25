caf52ac5a401e2c94923eba650c31f47a778388b
Crystal Liu <crl95@cornell.edu>
2018-04-25 03:29:55 -0400

    change frontend

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,4 +1,4 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from app.irsystem.models.search import *
@@ -10,29 +10,29 @@ net_id = "Ilan Filonenko: if56"
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search1')
-	query2 = request.args.get('search2')
+	who = request.args.get('who')
+	occasion = request.args.get('occasion')
+	min_price = request.args.get('min')
+	max_price = request.args.get('max')
 
-	# if not query1 and not query2:
-	# 	query = ""
-	# if query1 and not query2:
-	# 	query = query1 
-	# elif query2 and not query1:
-	# 	query = query2
-	# else :
-	# 	query = query1 + query2 
-	if not query and not query2:
+	if not who and not occasion:
+		query = ""
+	if who and not occasion:
+		query = who
+	elif occasion and not who:
+		query = occasion
+	else :
+		query = who + occasion
+
+	if not who and not occasion:
 		data = []
 		output_message = ''
 	else:
-		query = query + query2
-		output_message = "Your search: " + query + query2
-		names, prices, ratings = calc_sort(doc_by_vocab,query)
+		# query = query + occasion
+		output_message = "Your search: " + query
+		names, prices, ratings = calc_sort(doc_by_vocab,query,min_price,max_price)
 		data = []
 		for i in range(0,5):
 			triplet = [names[i],prices[i],ratings[i]]
 			data.append(triplet)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=query, data= data )
-
-
-
